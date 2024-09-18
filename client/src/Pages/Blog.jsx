2bf933746174a4d72ebcd7315c60b3386b007c5e
Abhishek <abhishek.s.yadav@slrtce.in>
2024-09-18 18:57:09 +0530

    some minor changes

--- client/src/Pages/Blog.jsx
+++ client/src/Pages/Blog.jsx
@@ -49,6 +49,38 @@ const posts = [
             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
       },
+      {
+        id: 1,
+        title: 'Boost your conversion rate',
+        href: '#',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        thumbnail: 'https://images.unsplash.com/photo-1726402919430-a4521f2b1fa6?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+          name: 'Michael Foster',
+          role: 'Co-Founder / CTO',
+          href: '#',
+          imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      },
+      {
+        id: 1,
+        title: 'Boost your conversion rate',
+        href: '#',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        thumbnail: 'https://images.unsplash.com/photo-1726402919430-a4521f2b1fa6?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+          name: 'Michael Foster',
+          role: 'Co-Founder / CTO',
+          href: '#',
+          imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      },
     
   ]
   
@@ -62,8 +94,8 @@ const posts = [
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {posts.map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between ">
-                <div className=''>
-                <img alt="" src={post.thumbnail} className="" />
+                <div>
+                <img alt="" src={post.thumbnail} className="rounded-3xl h-64 w-80" />
                 </div>
                 <div className="flex items-center gap-x-4 text-xs">
                   <time dateTime={post.datetime} className="text-gray-500">
